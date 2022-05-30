22e55d7a83568f0a1b8f35b275f555f11c61c917
David <davrui937@alu.centroafuera.es>
2022-05-30 12:09:17 +0200

    Login Correcto

---
+++ app/src/main/java/com/david/where2stop/SelectOptionAuthActivity.java
@@ -0,0 +1,40 @@
+package com.david.where2stop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SelectOptionAuthActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
+    Button mButtonGoToLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_option_auth);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Seleccionar opcion");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        mButtonGoToLogin = findViewById(R.id.btnGoToLogin);
+        
+        mButtonGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLogin();
+            }
+        });
+        
+    }
+
+    private void goToLogin() {
+        Intent intent = new Intent(SelectOptionAuthActivity.this,LoginActivity.class);
+        startActivity(intent);
+    }
+}
